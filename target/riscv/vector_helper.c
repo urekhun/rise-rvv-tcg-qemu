98ad7e4bcbac85b6f4aa959a73b30cc4b364badc
Paolo Savini <paolo.savini@embecosm.com>
2024-06-19 14:26:44 +0000

    [RISC-V] Simulate long vector load and store operations with builtin memcpy.
    This commit adds a call to __builtin_memcpy when the data to be
    loaded or stored exceeds 64 bits.
    The __builtin_memcpy intrinsic will move the data from the memory address
    of the simulated vector register to the destination memory address
    or vice versa.

--- target/riscv/vector_helper.c
+++ target/riscv/vector_helper.c
@@ -486,7 +486,22 @@ vext_group_ldst_host(CPURISCVState *env, void *vd, uint32_t byte_end,
     }
 
     fn = fns[is_load][group_size];
-    fn(vd, byte_offset, host + byte_offset);
+
+    if (byte_offset + 32 < byte_end) {
+      group_size = MO_256;
+      if (is_load)
+        __builtin_memcpy((uint8_t *)(vd + byte_offset), (uint8_t *)(host + byte_offset), 32);
+      else
+        __builtin_memcpy((uint8_t *)(host + byte_offset), (uint8_t *)(vd + byte_offset), 32);
+    } else if (byte_offset + 16 < byte_end) {
+      group_size = MO_128;
+      if (is_load)
+        __builtin_memcpy((uint8_t *)(vd + byte_offset), (uint8_t *)(host + byte_offset), 16);
+      else
+        __builtin_memcpy((uint8_t *)(host + byte_offset), (uint8_t *)(vd + byte_offset), 16);
+    } else {
+      fn(vd, byte_offset, host + byte_offset);
+    }
 
     return 1 << group_size;
 }
